54e3d538e0f5070c095a23c9f27e50f4dabc58b4
chaenning <codmsrjf@naver.com>
2025-07-29 14:14:37 +0900

    Create Character, GunBase
    - 기본 캐릭터 구현
    - 총 인터페이스 구현

---
+++ Source/XV/Private/XVCharacter.cpp
@@ -0,0 +1,160 @@
+#include "XVCharacter.h"
+#include "XVPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+// Sets default values
+AXVCharacter::AXVCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+		
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->TargetArmLength = 300.0f;  
+	SpringArmComp->bUsePawnControlRotation = true;  
+	
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
+	CameraComp->bUsePawnControlRotation = false;
+
+	NormalSpeed = 600.0f;
+	SprintSpeedMultiplier = 1.5f;
+	SprintSpeed = NormalSpeed * SprintSpeedMultiplier;
+
+	GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+}
+
+
+
+// Called to bind functionality to input
+void AXVCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+	    // IA를 가져오기 위해 현재 소유 중인 Controller를 ASpartaPlayerController로 캐스팅
+	    if (AXVPlayerController* PlayerController = Cast<AXVPlayerController>(GetController()))
+	    {
+	        if (PlayerController->MoveAction)
+	        {
+	            // IA_Move 액션 키를 "키를 누르고 있는 동안" Move() 호출
+	            EnhancedInput->BindAction(
+                    PlayerController->MoveAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AXVCharacter::Move
+                );
+	        }
+            
+	        if (PlayerController->JumpAction)
+	        {
+	            // IA_Jump 액션 키를 "키를 누르고 있는 동안" StartJump() 호출
+	            EnhancedInput->BindAction(
+                    PlayerController->JumpAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AXVCharacter::StartJump
+                );
+                
+	            // IA_Jump 액션 키에서 "손을 뗀 순간" StopJump() 호출
+	            EnhancedInput->BindAction(
+                    PlayerController->JumpAction,
+                    ETriggerEvent::Completed,
+                    this,
+                    &AXVCharacter::StopJump
+                );
+	        }
+            
+	        if (PlayerController->LookAction)
+	        {
+	            // IA_Look 액션 마우스가 "움직일 때" Look() 호출
+	            EnhancedInput->BindAction(
+                    PlayerController->LookAction,
+                    ETriggerEvent::Triggered,
+                    this,
+                    &AXVCharacter::Look
+                );
+	        }
+            
+	        if (PlayerController->SprintAction)
+	        {
+	            // IA_Sprint 액션 키를 "누르고 있는 동안" StartSprint() 호출
+	            EnhancedInput->BindAction(
+                    PlayerController->SprintAction,
+                    ETriggerEvent::Triggered, 
+                    this, 
+                    &AXVCharacter::StartSprint
+                );
+	            // IA_Sprint 액션 키에서 "손을 뗀 순간" StopSprint() 호출
+	            EnhancedInput->BindAction(
+                    PlayerController->SprintAction, 
+                    ETriggerEvent::Completed, 
+                    this, 
+                    &AXVCharacter::StopSprint
+                );
+	        }    
+	    }
+	}
+}
+
+void AXVCharacter::Move(const FInputActionValue& value)
+{
+	if (!Controller) return;
+	
+	const FVector2D MoveInput = value.Get<FVector2D>();
+
+	if (!FMath::IsNearlyZero(MoveInput.X))
+	{
+		AddMovementInput(GetActorForwardVector(), MoveInput.X);
+	}
+
+	if (!FMath::IsNearlyZero(MoveInput.Y))
+	{
+		AddMovementInput(GetActorRightVector(), MoveInput.Y);
+	}
+}
+
+void AXVCharacter::StartJump(const FInputActionValue& value)
+{
+	if (value.Get<bool>())
+	{
+		Jump();
+	}
+}
+
+void AXVCharacter::StopJump(const FInputActionValue& value)
+{
+	if (!value.Get<bool>())
+	{
+		StopJumping();
+	}
+}
+
+void AXVCharacter::Look(const FInputActionValue& value)
+{
+	FVector2D LookInput = value.Get<FVector2D>();
+	
+	AddControllerYawInput(LookInput.X);
+	AddControllerPitchInput(LookInput.Y);
+}
+
+void AXVCharacter::StartSprint(const FInputActionValue& value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+	}
+}
+
+void AXVCharacter::StopSprint(const FInputActionValue& value)
+{
+	if (GetCharacterMovement())
+	{
+		GetCharacterMovement()->MaxWalkSpeed = NormalSpeed;
+	}
+}
+
---
+++ Source/XV/Public/XVPlayerController.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "XVPlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class XV_API AXVPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AXVPlayerController();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputMappingContext* InputMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* JumpAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	UInputAction* LookAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+	UInputAction* SprintAction;
+
+	virtual void BeginPlay() override;
+};
