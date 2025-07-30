f073a7e07c2b8b2d9d311771176baa9cc60741f9
seowooyoung119 <seohascome20th@naver.com>
2025-07-30 17:50:04 +0900

    헤더 파일 누락
    헤더 파일 누락 등록

---
+++ Source/XV/Public/AI/AIComponents/AIConfigComponent.h
@@ -0,0 +1,62 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AIConfigComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class XV_API UAIConfigComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void ConfigSetting();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AttackRange;
+	
+	
+	// 귀 설정 (AI)
+public:
+	// 청각 범위
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AIHearingRange;
+	
+	// 청각 기억력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AIHearingSetMaxAge;
+	
+	// 눈 설정 (AI)
+public:
+	// 시야 범위
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AISightRadius;
+
+	// 시야를 잃는 범위
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AILoseSightRadius;
+
+	// 시야각
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AIPeripheralVisionAngleDegrees;
+
+	// 시야 기억력
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	float AISightSetMaxAge;
+	
+	// 적 감지
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	bool AIbDetectEnemies;
+
+	// 중립 감지
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	bool AIbDetectNeutrals;
+
+	// 아군 감지
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI | Setting")
+	bool AIbDetectFriendlies;
+};
---
+++ Source/XV/Public/AI/System/Task/Move/XVTASK_Patrol_Line.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "XVTASK_Patrol_Line.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XV_API UXVTASK_Patrol_Line : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UXVTASK_Patrol_Line();
+
+protected:
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector StartLocationKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector EndLocationKey;
+	
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TogglePatrolLocation
+;
+};
