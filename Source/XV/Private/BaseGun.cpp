8f9f985cd94605f4f66c9e722b271ddff057cd90
yoonsung00 <147009892+yoonsung00@users.noreply.github.com>
2025-08-04 11:35:18 +0900

    BaseGun 커밋

--- Source/XV/Private/BaseGun.cpp
+++ Source/XV/Private/BaseGun.cpp
@@ -4,6 +4,8 @@ ABaseGun::ABaseGun()
 {
 	PrimaryActorTick.bCanEverTick = false;
 	CurrentWeaponType = EWeaponType::None;
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("GunMesh"));
+	RootComponent = GunMesh;
 }
 
 EWeaponType ABaseGun::GetWeaponType()
@@ -17,6 +19,35 @@ void ABaseGun::BeginPlay()
 	
 }
 
+void ABaseGun::LoadWeaponData()
+{
+	if (!WeaponDataTable) return;
+
+	static const FString Context(TEXT("WeaponDataLoad"));
+	FWeaponStat* LoadedStat = WeaponDataTable->FindRow<FWeaponStat>(WeaponRowName, Context);
+
+	if (LoadedStat)
+	{
+		WeaponStat = *LoadedStat;
+
+		// 스켈레톤 메쉬 적용
+		if (WeaponStat.WeaponMesh)
+		{
+			GunMesh->SetSkeletalMesh(WeaponStat.WeaponMesh);
+		}
+
+		// 탄약, 데미지 등 설정
+		//AmmoCount = WeaponStat.MaxAmmo;
+		//FireRate = WeaponStat.FireRate;
+		//Damage = WeaponStat.Damage;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("WeaponStat not found for row: %s"), *WeaponRowName.ToString());
+	}
+}
+
+
 void ABaseGun::OnGunOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
@@ -28,6 +59,10 @@ void ABaseGun::OnGunEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* Othe
 }
 
 void ABaseGun::FireBullet()
+{
+}
+
+void ABaseGun::Reload()
 {
 }
 
