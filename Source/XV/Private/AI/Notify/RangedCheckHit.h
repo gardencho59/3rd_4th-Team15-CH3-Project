2d1a40774c952eaade86a3842162465885f602c2
seowooyoung119 <seohascome20th@naver.com>
2025-08-06 19:05:27 +0900

    ai 개선

--- Source/XV/Private/AI/Notify/RangedCheckHit.h
+++ Source/XV/Private/AI/Notify/RangedCheckHit.h
@@ -1,9 +1,11 @@
-﻿// RangedCheckHit.h
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Animation/AnimNotifies/AnimNotifyState.h"
 #include "RangedCheckHit.generated.h"
+
+class UNiagaraSystem;
+class USoundBase;
 
 UCLASS()
 class XV_API URangedCheckHit : public UAnimNotifyState
@@ -14,6 +16,7 @@ class XV_API URangedCheckHit : public UAnimNotifyState
 public:
 	// 애니메이션 노티파이 시 실행
 	virtual void NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float TotalDuration, const FAnimNotifyEventReference& EventReference) override;
+	void PlayHitEffects(UWorld* World, const FVector& SpawnLocation);
 
 	// 트레이스 박스 반 사이즈(직육면체 절반 크기)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RangeTrace")
@@ -26,4 +29,16 @@ public:
 	// 명중 확률 (0.0~1.0)
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "RangeTrace")
 	float HitProbability = 0.7f;
+
+	
+	// 총 쏘기 연출
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="VFX")
+	UNiagaraSystem* NiagaraEffect_Muzzle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="VFX")
+	UNiagaraSystem* NiagaraEffect_Flash;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="SFX")
+	USoundBase* HitSound;
+
 };
