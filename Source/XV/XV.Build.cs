2a1b544dd5d48661d6b68997e74d209fc3cb1dcf
SEOMAN <seohascome20th@naver.com>
2025-08-07 18:30:49 +0900

    Revert "UI"

--- Source/XV/XV.Build.cs
+++ Source/XV/XV.Build.cs
@@ -8,7 +8,7 @@ public class XV : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem", "GameplayTags", "Niagara","UMG","Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem", "GameplayTags", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
